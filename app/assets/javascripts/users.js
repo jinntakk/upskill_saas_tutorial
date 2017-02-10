732097b35f8e79c776e77d2014347bc21b0609f9
Jin Tak <takjinwoo@gmail.com>
2017-02-10 23:09:53 +0000

    Added server side code for creatingsucscriptions for pro users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,16 +2,16 @@
 //Document ready.
 $(document).on('turbolinks:load', function(){
   var theForm = $('#pro_form');
-  var SignupBtn = $('#form-signup-btn');
+  var submitBtn = $('#form-signup-btn');
   
-  //Set stripe public key.
-  Stripe.SetPublishableKey( $('meta[name="stripe-key"]').attr('content'));
+  //Set Stripe public key.
+  Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content'));
   
   //When user clicks form submit btn.
-  SignupBtn.click(function(event){
+  submitBtn.click(function(event){
     //Prevent default submission behavior.
     event.preventDefault();
-    SignupBtn.val("Processing").prop('disabled', true);
+    submitBtn.val("Processing").prop('disabled', true);
     
   //Collect the credit card fields.
   var ccNum = $('#card_number').val(),
@@ -19,7 +19,7 @@ $(document).on('turbolinks:load', function(){
       expMonth = $('#card_month').val(),
       expYear = $('#card_year').val();
       
-  //Use stripe js library to check for card errors.
+  //Use Stripe JS library to check for card errors.
   var error = false;
     
     //Validate card number.
@@ -42,7 +42,7 @@ $(document).on('turbolinks:load', function(){
     
   if (error) {
     //If there are errors don't send to stripe.
-    SignupBtn.prop('disabled', false).val('Sign Up');
+    submitBtn.prop('disabled', false).val('Sign Up');
   } else {   
     //Send the card info to stripe.
     Stripe.createToken({
@@ -63,8 +63,9 @@ $(document).on('turbolinks:load', function(){
     
     //Inject the token in a hidden field.
     theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
-  }
-  
+    
+      
   //Submit forms to our rails app.
   theForm.get(0).submit();
+  }
 });
